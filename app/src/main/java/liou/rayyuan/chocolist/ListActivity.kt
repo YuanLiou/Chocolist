0e122481d0ced199c4a52eafac42caead32a99f7
Ray-Yuan Liu <louis383@gmail.com>
2018-05-30 10:51:47 +0800

    git init

---
+++ app/src/main/java/liou/rayyuan/chocolist/ListActivity.kt
@@ -0,0 +1,12 @@
+package liou.rayyuan.chocolist
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list)
+    }
+}
